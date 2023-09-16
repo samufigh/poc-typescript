c3fb9de8655c62f72e09b6df52322f8757fa9804
samufigh <samufigh21@gmail.com>
2023-09-16 20:20:18 -0300

    fix: messages

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -4,31 +4,43 @@ import httpStatus from "http-status";
 import { taskService } from "@/services/taskService";
 
 
-async function createTask(req :Request, res :Response) {
+async function createTask(req: Request, res: Response) {
 
     const infoTask = req.body as CreateTask
     await taskService.createTask(infoTask)
-    res.sendStatus(httpStatus.CREATED)
+    res.status(httpStatus.CREATED).send(
+        {
+            "message": "Tarefa criada com sucesso"
+        }
+    )
 }
 
-async function getTasks(req :Request, res :Response) {
+async function getTasks(req: Request, res: Response) {
 
     const tasks = await taskService.getTasks()
     res.status(200).send(tasks)
 }
 
-async function updateTask(req :Request, res :Response) {
+async function updateTask(req: Request, res: Response) {
 
-    const {id} = req.params
+    const { id } = req.params
     const infoTask = req.body as Task
     await taskService.updateTask(infoTask, id)
-    res.sendStatus(httpStatus.OK)
+    res.status(httpStatus.OK).send(
+        {
+            "message": "Tarefa atualizada com sucesso"
+        }
+    )
 }
 
-async function deleteTask(req :Request, res :Response) {
-    const {id} = req.params
+async function deleteTask(req: Request, res: Response) {
+    const { id } = req.params
     await taskService.deleteTask(id)
-    res.sendStatus(httpStatus.OK)
+    res.status(httpStatus.OK).send(
+        {
+            "message": "Tarefa excluída com sucesso"
+        }
+    )
 }
 
 export const taskController = { createTask, getTasks, updateTask, deleteTask }
